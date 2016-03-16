2d7b20200556fa1fc5dc00c475c7bee74b4aecfe
WebDevJL <webdev.jl@gmail.com>
2016-03-16 06:25:44 +0100

    build3 3: first test

---
+++ tests/System/Web/ServerContextTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @locked
+ * @since Test Suite v1.2.0
+ */
+
+namespace Puzzlout\FrameworkMvc\Tests\System\Web;
+
+use Puzzlout\FrameworkMvc\System\Web\ServerContext;
+
+class ServerContextTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Initialize the app object.
+     */
+    protected function setUp() {
+        
+    }
+
+    /**
+     * This method is generated.
+     */
+    public function testInstanceIsCorrect() {
+        $result = new ServerContext();
+        $this->assertInstanceOf('Puzzlout\FrameworkMvc\System\Web\ServerContext', $result);
+    }
+
+    //Write the next tests below...
+}
