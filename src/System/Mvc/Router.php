4e9f902f510b4036aa600a4fd218813bb157a50b
WebDevJL <webdev.jl@gmail.com>
2016-04-07 07:15:10 +0200

    updating Router code and tests

--- src/System/Mvc/Router.php
+++ src/System/Mvc/Router.php
@@ -5,6 +5,9 @@ namespace Puzzlout\FrameworkMvc\System\Mvc;
 use Puzzlout\FrameworkMvc\System\Web\HttpRequest\RequestBase;
 use Puzzlout\FrameworkMvc\System\Web\HttpRequest\ServerContext;
 use Puzzlout\FrameworkMvc\PhpExtensions\ServerConst;
+use Puzzlout\FrameworkMvc\Commons\Validation\StringValidator;
+use Puzzlout\Exceptions\Classes\Core\InvalidArgumentException;
+use Puzzlout\Exceptions\Codes\LogicErrors;
 
 /**
  * Route class
@@ -24,25 +27,83 @@ class Router {
      * @var Puzzlout\FrameworkMvc\System\Web\HttpRequest\RequestBase 
      */
     private $Request;
-    
+
     /**
      * The list of Routes computed.
      * 
      * @var array
      */
-    private $Routes;
+    protected $Routes;
 
+    /**
+     * The constructor initializing the Request and Routes members.
+     * 
+     * @param \Puzzlout\FrameworkMvc\System\Web\HttpRequest\RequestBase $request
+     */
     public function __construct(RequestBase $request) {
         $this->Request = $request;
         $this->Routes = [];
     }
 
+    /**
+     * Instantiate the class for use of chain method calls.
+     * 
+     * @param \Puzzlout\FrameworkMvc\System\Web\HttpRequest\RequestBase $request
+     * @return \Puzzlout\FrameworkMvc\System\Mvc\Router
+     */
     public static function Init(RequestBase $request) {
         $instance = new Router($request);
         return $instance;
     }
 
-    
+    /**
+     * Getter of Routes member.
+     * 
+     * @return array
+     */
+    public function routes() {
+        return $this->Routes;
+    }
+
+    /**
+     * Count the number of routes stored in the member Routes.
+     * 
+     * @return int
+     */
+    public function routeCount() {
+        return count($this->Routes);
+    }
+
+    /**
+     * Retrieve a route for a given index.
+     * 
+     * @param int $index The index searched.
+     * @return Puzzlout\FrameworkMvc\System\Mvc\Route The route matching the index.
+     * @throws InvalidArgumentException When the $index parameter value is not found in the Routes member.
+     */
+    public function getRouteAtIndex($index) {
+        if (!isset($this->Routes[$index])) {
+            $err = "The index '$index' was not found in the Routes list.";
+            throw new InvalidArgumentException($err, LogicErrors::PARAMETER_VALUE_INVALID, null);
+        }
+        return $this->Routes[$index];
+    }
+
+    /**
+     * Retrieve a route for a given key.
+     * 
+     * @param string $key The key searched.
+     * @return Puzzlout\FrameworkMvc\System\Mvc\Route The route matching the key.
+     * @throws InvalidArgumentException When the $key parameter value is not found in the Routes member.
+     */
+    //public function getRouteAtKey($key) {
+    //    if (!isset($this->Routes[$key])) {
+    //        $err = "The key '$key' was not found in the Routes list.";
+    //        throw new InvalidArgumentException($err, LogicErrors::PARAMETER_VALUE_INVALID, null);
+    //    }
+    //    return $this->Routes[$key];
+    //}
+
     public function findRoute() {
         $getRouteRequest = $this->buildGetRouteRequest();
         $route = Route::init($getRouteRequest)->fill();
@@ -55,8 +116,8 @@ class Router {
      * @return \Puzzlout\FrameworkMvc\System\Mvc\GetRouteRequest
      */
     protected function buildGetRouteRequest() {
-        $finalUri = $this->extractQueryStringFreeUri();
-        $appAlias = $this->request->appAlias();
+        $finalUri = $this->extractCleanUri();
+        $appAlias = $this->Request->appAlias();
         $getRouteRequest = new GetRouteRequest($appAlias, $finalUri);
         return $getRouteRequest;
     }
@@ -68,7 +129,7 @@ class Router {
      * @throws RuntimeException When the URI is null or empty.
      * @todo Create error code
      */
-    protected function extractQueryStringFreeUri() {
+    protected function extractCleanUri() {
         $rawUri = $this->Request->serverContext()->getValueFor(ServerContext::INPUT_SERVER, ServerConst::REQUEST_URI);
         $uriWithoutQueryString = strtok($rawUri, '?');
         $uriWithHash = strtok($uriWithoutQueryString, '#');
--- tests/System/Mvc/RouterTest.php
+++ tests/System/Mvc/RouterTest.php
@@ -9,17 +9,21 @@ namespace Puzzlout\FrameworkMvc\Tests\System\Mvc;
 
 use Puzzlout\FrameworkMvc\System\Mvc\Router;
 use Puzzlout\FrameworkMvc\System\Web\HttpRequest\RequestBase;
+use Puzzlout\FrameworkMvc\System\Web\HttpRequest\ServerContext;
+use Puzzlout\FrameworkMvc\PhpExtensions\ServerConst;
 use Puzzlout\FrameworkMvc\Tests\MockingHelpers\UnitTestHelper;
 
 class RouterTest extends \PHPUnit_Framework_TestCase {
 
     private $request;
+    private $inputs;
 
     /**
      * Initialize the app object.
      */
     protected function setUp() {
-        $this->request = new RequestBase(UnitTestHelper::simulationRealValidInputs());
+        $this->inputs = UnitTestHelper::simulationRealValidInputs();
+        $this->request = new RequestBase($this->inputs);
     }
 
     /**
@@ -28,6 +32,8 @@ class RouterTest extends \PHPUnit_Framework_TestCase {
     public function testInstanceIsCorrect() {
         $result = new Router($this->request);
         $this->assertInstanceOf('Puzzlout\FrameworkMvc\System\Mvc\Router', $result);
+        $this->assertTrue(is_array($result->routes()));
+        $this->assertEmpty($result->routes());
     }
 
     public function testInstanceWithInit() {
@@ -37,4 +43,59 @@ class RouterTest extends \PHPUnit_Framework_TestCase {
     }
 
     //Write the next tests below...
+    public function testExtractCleanUriWithEmtpyValue() {
+        
+    }
+
+    public function testExtractCleanUriWithNullValue() {
+        
+    }
+
+    public function testExtractCleanUriWithValidValue() {
+        $this->inputs[ServerContext::INPUT_SERVER][ServerConst::REQUEST_URI] = '/App/Controller/Action?querystring=1';
+        $this->request = RequestBase::init($this->inputs)->fill();
+        $instance = new Router($this->request);
+        $instance->findRoute();
+        $this->assertCount(1, $instance->routes());
+        return $instance;
+    }
+
+    public function testRouteCountEquals0() {
+        $instance = new Router($this->request);
+        $this->assertCount(0, $instance->routes());
+    }
+
+    public function testRouteCountIsGreaterThan0() {
+        $this->inputs[ServerContext::INPUT_SERVER][ServerConst::REQUEST_URI] = '/App/Controller/Action?querystring=1';
+        $this->request = RequestBase::init($this->inputs)->fill();
+        $instance = new Router($this->request);
+        $instance->findRoute();
+        $this->assertTrue($instance->routeCount() === 1);
+    }
+
+    public function testGetRouteAtValidIndex() {
+        $this->inputs[ServerContext::INPUT_SERVER][ServerConst::REQUEST_URI] = '/App/Controller/Action?querystring=1';
+        $this->request = RequestBase::init($this->inputs)->fill();
+        $instance = new Router($this->request);
+        $instance->findRoute();
+        $routeCount = $instance->routeCount();
+        $index = rand(0, $routeCount - 1);
+        $route = $instance->getRouteAtIndex($index);
+        $this->assertInstanceOf('\Puzzlout\FrameworkMvc\System\Mvc\Route', $route);
+        $this->assertSame('controller', $route->controller());
+        $this->assertSame('action', $route->action());
+        return $instance;
+    }
+
+    public function testGetRouteAtInvalidIndex() {
+        $instance = $this->testGetRouteAtValidIndex();
+        try {
+            $routeCount = $instance->routeCount();
+            $index = rand($routeCount, $routeCount * 10);
+            $instance->getRouteAtIndex($index);
+        } catch (\Puzzlout\Exceptions\Classes\Core\InvalidArgumentException $exc) {
+            $this->assertInstanceOf('Puzzlout\Exceptions\Classes\Core\InvalidArgumentException', $exc);
+        }
+    }
+
 }
