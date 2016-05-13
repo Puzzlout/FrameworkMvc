445fc40525ff0bda753bde7a0270f56b4504ab39
WebDevJL <webdev.jl@gmail.com>
2016-05-13 06:50:14 +0200

    testing ArrayListAlgorithm class

--- tests/Commons/FileManager/Algorithms/ArrayListAlgorithmTest.php
+++ tests/Commons/FileManager/Algorithms/ArrayListAlgorithmTest.php
@@ -10,31 +10,35 @@ namespace Puzzlout\FrameworkMvc\Tests\Commons\FileManager\Algorithms;
 use Puzzlout\FrameworkMvc\Commons\FileManager\Algorithms\ArrayListAlgorithm;
 
 class ArrayListAlgorithmTest extends \PHPUnit_Framework_TestCase {
-  /**
-   * Initialize the app object.
-   */
-  protected function setUp()
-  {
-  }
-  
-  /**
-   * This method is generated.
-   */
-  public function testInstanceIsCorrect()
-  {
-    $instance = new ArrayListAlgorithm();
-    $this->assertInstanceOf('Puzzlout\FrameworkMvc\Commons\FileManager\Algorithms\ArrayListAlgorithm', $instance);
-  }
-  
-  public function testInstanceWithInit() {
-      $instance = ArrayListAlgorithm::init();
-      $this->assertInstanceOf('Puzzlout\FrameworkMvc\Commons\FileManager\Algorithms\ArrayListAlgorithm', $instance);
-      return $instance;
-  }
 
-  
-  //Write the next tests below...
-  public function test() {
-      $this->assertCount(2, $this->testInstanceWithInit()->excludeList());
-  }
+    /**
+     * Initialize the app object.
+     */
+    protected function setUp() {
+        
+    }
+
+    /**
+     * This method is generated.
+     */
+    public function testInstanceIsCorrect() {
+        $instance = new ArrayListAlgorithm();
+        $this->assertInstanceOf('Puzzlout\FrameworkMvc\Commons\FileManager\Algorithms\ArrayListAlgorithm', $instance);
+    }
+
+    public function testInstanceWithInit() {
+        $instance = ArrayListAlgorithm::init();
+        $this->assertInstanceOf('Puzzlout\FrameworkMvc\Commons\FileManager\Algorithms\ArrayListAlgorithm', $instance);
+        return $instance;
+    }
+
+    //Write the next tests below...
+    public function testExcludeList() {
+        $this->assertCount(2, $this->testInstanceWithInit()->excludeList());
+    }
+
+    public function testExcludeListForTestSuite() {
+        $this->assertCount(7, $this->testInstanceWithInit()->excludeListForTestSuite());
+    }
+
 }
